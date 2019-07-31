659753c1bc2fe52f00f7d401dc9199cf1f19b7a3
YangTzChing <rsrs6543210@gmail.com>
2019-07-31 17:39:59 +0800

    Done All Function is developed!!!!!!

--- Server/modifyname_page.php
+++ Server/modifyname_page.php
@@ -1,7 +1,11 @@
 <?php session_start();
-    $oldname = 'upload/'.$_POST['oldname'];
-    $modifyname='upload/'.$_POST['modifyname'].'.png';
-?>
+    $person=$_SESSION['person'];
+    $sql_old=$_POST['oldname'];
+    $sql_new=$_POST['modifyname'] ;
+
+    $rename_old='upload/'.$sql_old;
+
+    ?>
 
 <?php
     //Connect to Database setting
@@ -11,14 +15,32 @@
     $db_name = "RegistrationForm";
 
     $con =new mysqli($server,$username,$password,$db_name);
-    echo 'Old file name: '.$oldname.'<br>';
-    echo 'New file name: '.$modifyname.'<br><br>';
 
-    if (rename($oldname,$modifyname)){
-        echo 'Change success !';
+    // Get file's data first
+    $sql = "Select * from FileData where Filename='$sql_old'";
+    $query = $con->query($sql);
+    $row = $query->fetch_assoc();
+
+    $rename_new='upload/'.$sql_new;
+
+    if (rename($rename_old,$rename_new)){
+        echo 'Success <br>' ;
     }else{
-        echo  'Failure';
+        echo 'Fail <br>' ;
+    }
+
+    $sql = "Update FileData Set Filename='$sql_new' where Filename='$sql_old'";
+    $query = $con->query($sql);
+
+    if($query === true){
+        echo 'File Name Update Success.'.'<br>';
+        echo 'Return in 3 seconds...';
+
+        echo "<meta http-equiv=REFRESH CONTENT=3;url=personal_page.php?id=$person>";
+    }else{
+        echo "Error updating record: " . $con->error.'<br>';
+        echo "<meta http-equiv=REFRESH CONTENT=3;url=personal_page.php?id=$person>";
+
     }
 
 ?>
-
